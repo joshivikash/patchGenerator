e6c2c183534bba697cb534e513cff9a2be5b0288
Vikash Joshi <joshi.vikash17@gmail.com>
2017-11-16 19:01:20 +0530

    Modified install.sh to make it working

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+rm -R -f linux_release
 unzip ServerPatch
 # Copying the files from ServerPatch.zip to /var/pari/dash
-for file in `unzip -Z -1 ServerPatch`; do
-	\cp -f $line \var\pari\dash
+for line in `unzip -Z -1 ServerPatch`; do
+        line="$(echo $line | sed 's#\\#\/#g#')"
+        newLine="$(echo $line | cut -c 15-)"
+	\cp -f $line '/var/pari/dash/'$newLine
 done
 # Executing commands from 'instructions' file
 bash instructions.txt
