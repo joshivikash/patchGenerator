819b16d4719e97155ccdd3c3aca3df86e0674e6c
Vikash Joshi <joshi.vikash17@gmail.com>
2017-12-11 19:56:04 +0530

    checking for xml and properties file and merging them, and if its an sql file then execute them

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@
 rm -R -f linux_release
 unzip ServerPatch
 # Copying the files from ServerPatch.zip to /var/pari/dash
-for line in `unzip -Z -1 ServerPatch`; do
-        line="$(echo $line | sed 's#\\#\/#g#')"
+for file in `unzip -Z -1 ServerPatch`; do
+        file="$(echo $line | sed 's#\\#\/#g#')"
         newLine="$(echo $line | cut -c 15-)"
+# If the file is a properties or xml file, and if it exists in both places, then merge them
 	\cp -f $line '/var/pari/dash/'$newLine
 done
 # Executing commands from 'instructions' file
